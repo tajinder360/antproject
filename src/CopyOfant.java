98d55f7b1e2e63e96006ec29df7f190305faeaef
Tajinder Pal Singh <tajinders@360logica.com>
2017-10-11 15:34:36 +0530

    updated code

---
+++ src/CopyOfant.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class CopyOfant {
+	
+	
+	@Test
+	
+	public void display()
+	{
+		
+		System.out.println("Run the second ant program");
+		
+	}
+	
+
+}
